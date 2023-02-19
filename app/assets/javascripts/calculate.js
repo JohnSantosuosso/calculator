6d0351e7911e7127de9c5525e384427f32dea5b7
John Santosuosso <john.santosuosso1@gmail.com>
2023-02-19 17:09:10 -0500

    modify numerical display create javascript file

---
+++ app/assets/javascripts/calculate.js
@@ -0,0 +1,7 @@
+//query selector for buttons class (include equal/clear/symbols?)
+//query selector for div rectangle's element inside div rectangle '0'
+//event listener on click event listening for click event inside buttons class
+//variable function to store value of the event click and replace 0 with that value (show button click)
+//variable function to keep ongoing string of what is occuring
+//variable function to calculate total when equal button is pressed (turn string into math equation)
+const calcButtons = document.querySelectorAll('.buttons');
