420b91aa04df1b39da3e462516b9bd9431ad6415
John Santosuosso <john.santosuosso1@gmail.com>
2023-02-21 15:30:07 -0500

    add additional logic to calculator

--- app/assets/javascripts/calculate.js
+++ app/assets/javascripts/calculate.js
@@ -4,4 +4,64 @@
 //variable function to store value of the event click and replace 0 with that value (show button click)
 //variable function to keep ongoing string of what is occuring
 //variable function to calculate total when equal button is pressed (turn string into math equation)
-const calcButtons = document.querySelectorAll('.buttons');
+console.log('Javascript loaded');
+
+const buttonValues = [];
+
+const total = [];
+
+const transformation = [];
+
+const calcButtons = document.querySelectorAll('.buttons button');
+
+calcButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.textContent;
+    if (['+', '-', '*', '/'].includes(buttonValue)) {
+      // If the buttonValue is an operator, do something
+      console.log('Operator button clicked');
+      // combine buttonValues
+      calculateInputValues(buttonValues);
+      transformation.push(buttonValue);
+      buttonValues.length = 0;
+      console.log(transformation);
+    } else if (buttonValue === '=') {
+      // If the buttonValue is the equal sign, do the calculation
+      console.log('Equal button clicked');
+      calculateInputValues(buttonValues);
+      calculateTotal(transformation);
+      buttonValues.length = 0;
+    } else {
+      // If the buttonValue is not an operator, do something else
+      console.log('Non-operator button clicked');
+      //add buttonValues to array
+      buttonValues.push(buttonValue);
+    }
+  });
+});
+
+function calculateInputValues(buttonValues) {
+  const concatenatedValue = buttonValues.join('');
+  const numericValue = Number(concatenatedValue);
+  transformation.push(numericValue);
+};
+
+function calculateTotal(transformation) {
+  console.log(transformation);
+  let result = transformation[0];
+  for (let i = 1; i < transformation.length; i += 2) {
+    const operator = transformation[i];
+    const operand = transformation[i + 1];
+
+    if (operator === '+') {
+      result += operand;
+    } else if (operator === '-') {
+      result -= operand;
+    } else if (operator === '*') {
+      result *= operand;
+    } else if (operator === '/') {
+      result /= operand;
+    }
+  };
+  console.log(result);
+}
